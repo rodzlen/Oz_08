4927f55ea766449003a94719c771309a195ce34f
rodzlen <qkqhrkxdkdy@naver.com>
2024-11-21 22:47:58 +0900

    vod 메모 수정

--- python/Day04/vod/vod.py
+++ python/Day04/vod/vod.py
@@ -41,46 +41,63 @@
 
 
 
-boong = {
-    "팥붕": { 
-        "가격" : 2000,
-        "수량":0},
-    "슈붕":{
-        "가격" : 2500,
-        "수량" : 0},
-    "잡붕":{
-        "가격": 3000,
-        "수량": 0}
-}
+# boong = {
+#     "팥붕": { 
+#         "가격" : 2000,
+#         "수량":0},
+#     "슈붕":{
+#         "가격" : 2500,
+#         "수량" : 0},
+#     "잡붕":{
+#         "가격": 3000,
+#         "수량": 0}
+# }
 
-# 방문이력 확인
-history=False
+# # 방문이력 확인
+# history=False
 
-while True :
-    sum =0 # 총 개수
-    total = 0 # 총 가격
-    ment=""# 결제 안내 멘트
+# while True :
+#     sum =0 # 총 개수
+#     total = 0 # 총 가격
+#     ment=""# 결제 안내 멘트
     
-    # 방문이력에 따른 멘트
-    if history == True :
-        print("또 오셨네요! 붕어빵 인터내셔널입니다!")
-    else:    
-        history = True
-        print("환영합니다 고객님 붕어빵 인터내셔널입니다!")
+#     # 방문이력에 따른 멘트
+#     if history == True :
+#         print("또 오셨네요! 붕어빵 인터내셔널입니다!")
+#     else:    
+#         history = True
+#         print("환영합니다 고객님 붕어빵 인터내셔널입니다!")
 
-    # 제품별 구매 수량 입력
-    for i in boong:
-        count = int(input(f"구매할 {i}의 개수를 입력하세요"))
-        boong[i]["수량"] += count
-        sum += count
-        total += count * boong[i]["가격"]
-        ment += f"{i}{count}개, "
+#     # 제품별 구매 수량 입력
+#     for i in boong:
+#         count = int(input(f"구매할 {i}의 개수를 입력하세요"))
+#         boong[i]["수량"] += count
+#         sum += count
+#         total += count * boong[i]["가격"]
+#         ment += f"{i}{count}개, "
 
-    # 구매수량 선택 후 결제금액 안내
-    print(f"{ment}총 {sum}개 {total}원입니다.")
+#     # 구매수량 선택 후 결제금액 안내
+#     print(f"{ment}총 {sum}개 {total}원입니다.")
 
-    # 재구매 의사 묻기
-    again = input("구매를 계속 하시겠습니까? 1. 네, 2. 아니오")
-    if again == "2":
-        print("감사합니다. 또 오세요!")
-        break
+#     # 재구매 의사 묻기
+#     again = input("구매를 계속 하시겠습니까? 1. 네, 2. 아니오")
+#     if again == "2":
+#         print("감사합니다. 또 오세요!")
+#         break
+
+# 오피스텔 = [[201,202],[301,302],[401,402]]
+# for i in range(len(오피스텔)):
+#     for j in range(2):
+#         print(오피스텔[i][j])
+#     print('-----')
+
+# ohlc = [["open", "high", "low", "close"], [100, 110, 70, 100], [200, 210, 180, 190], [300, 310, 300, 310]]
+
+# for i in range(1,4):
+#     if ohlc[i][3] > 100:
+#         print(ohlc[i])    
+
+x = 17
+a = list(map(int,str(x)))
+result = x % sum(a) == 0
+print(f'{x} {result}')
