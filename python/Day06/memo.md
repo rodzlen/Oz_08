8fd3c6d2b345c6a8402c2699747b1fb3c576f1bd
rodzlen <qkqhrkxdkdy@naver.com>
2024-11-26 01:12:55 +0900

    Update Day06 memo

--- python/Day06/memo.md
+++ python/Day06/memo.md
@@ -72,3 +72,95 @@ x, y = tuple()
 ```
 #### enumerate()
 괄호를 제거한 값을 튜플로 반환
+---
+
+
+## 람다(Lamda)
+함수 자체를 매개변수로 사용하기 쉽도록 하는 기능
+
+### 콜백함수
+함수의 매겨변수로 사용되는 함수
+
+```python
+def call(function) # 보통 콜백 파라미터로 받을 때 function 씀
+    for i in range(5):
+        function()
+
+def talk():
+    print("파이썬 이건 또 무슨개념?")
+
+call(talk)
+```
+
+**map()** : 리스트의 요소를 함수에 넣고 리턴된 값으로 새로운 리스트를 구성
+
+```python
+# 자기 자신을 ㄱ곱한 결과를 새로운 list로 만들어 주는 코드
+def square(i):
+    return i*i
+numbers_list=[1,2,3,4,5,6,7,8,9,10]
+
+result = map(square, numbers_list)
+```
+----
+**filter()** : 리스트의 요소에 조건을 가진 함수를 넣고 True가 반환되는 요소를 반환
+```python
+def under(i):
+    return i<5
+result =filter(under, numbers_list)
+```
+해당 정보는 오브젝트로 반환된다. list()를 사용해서 형변환 해줘야 한다.
+
+**lamda 사용**
+
+map() 사용에 적용한 예
+```python
+square = lambda i : i * i
+numbers_list=[1,2,3,4,5,6,7,8,9,10]
+
+result = map(square, numbers_list)
+```
+
+filter()사용에 적용한 예
+
+```python
+under = lambda i : i < 5
+numbers_list=[1,2,3,4,5,6,7,8,9,10]
+
+result = filter(under, numbers_list)
+```
+## 제너레이터 
+iterator를 생성하는 객체
+iterator : next메소드를 갖고있고 순차적으로 다음 값을 리턴하는 객체
+
+제너레이터는 함수안에 yield 키워드를 사용하여 만든다.
+함수 안에 yield 키워드가 존재하기만 해도 제너레이터로 인식된다
+
+제너레이터함수로 인식하면 호출해도 함수 내부 코드가 작동하지 않는다.
+
+```python
+def test():
+    print("ㅈㅔ너레이터 함수")
+    yield "test"
+```
+- 출력 실행 안됨
+
+## 파이썬 타입 힌트
+동적 프로그래밍 언어
+
+인터프리터 언어인 파이썬은 코드 실행시 데이터 타입을 추론한다. 파이썬은 데이터 타입이 정해져 있지 않기에 개발자가 원하면 언제든 바꿀 수 있다. 
+
+때문에 소규모 프로젝트나 일회성 스크립트에서는 유연하기 때문에 빠르게 진행할 수 있다. 
+
+하지만 프로젝트의 규모가 커지면 안정성이 약하여 치명적인 버그로 이어질 확률이 높다.
+
+타입 힌팅 이라는 개발 프로세스를 도입해 사용할 수 있다.
+
+### 변수 타입 어노테이션
+3.x 버전에서 사용됨 
+
+### 함수 타입 어노테이션
+인자 타입과 반환 타입 두 곳에 추가 가능 
+인자에 타입 어노테이션을 추가할 때는 변수와 동일한 문법을 사용
+
+반환값에 대한 타입을 추가할 떄는 -> 사용
--- python/Day06/vod/vod.py
+++ python/Day06/vod/vod.py
@@ -1,40 +1,76 @@
-# ##start end mul
-# result = []
-# def cal(start, end, mul):
-#     for i in range(start, end, mul):
-#         result.append(i)
-#     return print(result)
-# cal(1,10,3)
+# # ##start end mul
+# # result = []
+# # def cal(start, end, mul):
+# #     for i in range(start, end, mul):
+# #         result.append(i)
+# #     return print(result)
+# # cal(1,10,3)
 
-# 팩토리얼 구하기
+# # 팩토리얼 구하기
 
 
-def fac(n):
-    if n == 0:
-        return 1
-    else:
-        return n * fac(n-1)
+# def fac(n):
+#     if n == 0:
+#         return 1
+#     else:
+#         return n * fac(n-1)
     
 
-print(fac(4))
+# print(fac(4))
 
-## 피보나치 
-count = 0
-memo ={
-    1:1,
-    2:1
-    }
+# ## 피보나치 
+# count = 0
+# memo ={
+#     1:1,
+#     2:1
+#     }
 
-def fi(n):
-    print(f"피보나치 수열{n} 구합니다.")
-    global count
-    count += 1
-    if n in memo:
-        return memo[n]
-    output = fi(n-1)+ fi(n-2)
-    memo[n] = output
-    return output
-n = int(input("피보나치 수열을 구할 숫자를 입력"))
-fi(n)
-print(f'피보나치 수열{n}을 구하기 위해 {count}번 계산되었습니다')
-print(memo)
+# def fi(n):
+#     print(f"피보나치 수열{n} 구합니다.")
+#     global count
+#     count += 1
+#     if n in memo:
+#         return memo[n]
+#     output = fi(n-1)+ fi(n-2)
+#     memo[n] = output
+#     return output
+# n = int(input("피보나치 수열을 구할 숫자를 입력"))
+# fi(n)
+# print(f'피보나치 수열{n}을 구하기 위해 {count}번 계산되었습니다')
+# print(memo)
+
+
+# 제너레이터 
+
+def test():
+    print("ㅈㅔ너레이터 함수")
+    yield 1
+    print("이것도 두번쨰 절에 포함일까?")
+    print("두 번째 출력")
+    yield 2
+
+output = test()
+print("세번째 출력")
+
+x = next(output)
+print("네 번째 출력")
+print(x)
+y= next(output)
+print(y)
+print("e다섯번쨰")
+z = next(output)
+
+my_dog : str = "hunt"
+my_dog : int = 7
+
+dogs : list[str] = ["닥스훈트", "시바견", "웰시코기"]
+dogs : dict[str, int] = {
+    "닥스훈트"  : 21
+}
+
+def hunt(name: str) -> str:
+    return name[0]
+def hunt_info(name: str, age : int= 7)-> str:
+    return name + str(age)
+def hunt_happy(name:str) -> None:
+    print("간식줄게"+name)
