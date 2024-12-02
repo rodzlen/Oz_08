af770057c7185b5f8aaf82dfa2ac96ca59ae817d
rodzlen <qkqhrkxdkdy@naver.com>
2024-12-02 15:08:38 +0900

    📝 Connect database
    mydata베이스 연동 및 가상 더미 데이터 생성을 위한 faker모듈 연결

--- database/Day03/vod/data_generate.py
+++ database/Day03/vod/data_generate.py
@@ -1,1 +1,15 @@
-data_generate.py
+import mysql.connector # 데이터베이스 커넥터 임포트
+import random # 내장
+from faker import Faker # 더미데이터 라이브러리
+
+# mysql 연결 설정
+db_connection = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='1234', # 문자열로 넣어야함
+    database='mydatabase'
+)
+
+# mysql 연결
+cursor = db_connection.cursor()
+fake = Faker()
