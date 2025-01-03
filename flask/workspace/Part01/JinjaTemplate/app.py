ed05361e4489ab02bdf88b747a4a5a0a736ff82e
rodzlen <qkqhrkxdkdy@naver.com>
2025-01-03 17:40:40 +0900

    📝 Route 사용하여 데이터 넘기기 실습

---
+++ flask/workspace/Part01/JinjaTemplate/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    data ={
+        'title': "Flask Jinja Template",
+        'user': 'hwisu',
+        'items': ['item1','item2','item3']
+    }
+    # rendering할 html 파일명 입력
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
