7007d6433213beff48654ce81d8491744e00d10e
rodzlen <qkqhrkxdkdy@naver.com>
2025-01-07 14:41:55 +0900

    📝 Api 설정
    openAPI api에 bluepirnt 등록

--- flask/workspace/Part03/ORM/app.py
+++ flask/workspace/Part03/ORM/app.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from flask_smorest import Api
 from db import db
-from models import User, Board
+from routes.users import user_blp
+from routes.board import board_blp
+
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1234@localhost/oz'
@@ -19,23 +22,20 @@ app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
 app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
 api = Api(app)
-#api.register_blueprint()
+
 
 from flask import render_template
 
 @app.route('/manage-boards')
 def manage_boards():
-    return render_template('board.html')
+    return render_template('boards.html')
 
 @app.route('/manage-users')
 def manage_users():
     return render_template('users.html')
 
-from routes.board import board_blp
-from routes.users import user_blp
-app.register_blueprint(board_blp)
-app.register_blueprint(user_blp)
-
+api.register_blueprint(board_blp)
+api.register_blueprint(user_blp)
 
 if __name__ == '__main__':
     with app.app_context():
