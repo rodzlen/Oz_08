7b70023f2b4012d372b9fc362333063df262f28b
rodzlen <qkqhrkxdkdy@naver.com>
2025-01-07 14:06:11 +0900

    📝 Model 추가
    User, Board 테이블 추가

---
+++ flask/workspace/Part03/ORM/models.py
@@ -0,0 +1,23 @@
+# Model은 Table 생성 
+# board (모델은 하나니까 단수임)
+# user
+
+from db import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable =False)
+    email = db.Column(db.String(100), unique=True, nullable = False)
+    boards = db.relationship('Board', back_populates ='author', lazy='dynamic') # workbench에서는 보이지 않음 관계를 그냥 정의했을 뿐임
+
+class Board(db.Model):
+    __tablename__ = 'boards'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100),nullable=False)
+    content = db.Column(db.String(300))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    author = db.relationship('User', back_populates = 'boards')
+
